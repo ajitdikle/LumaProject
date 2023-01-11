03b3513786a68374e98ac2034f59c782ae2f5330
ajitd <ajitdikle1309@gmail.com>
2023-01-11 16:47:40 +0530

    update

---
+++ MagentoLuma/Dashboard.cs
@@ -0,0 +1,29 @@
+﻿using MagentoLuma.Base;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagentoLuma
+{
+    public class Dashboard : AutomationWrapper
+    {
+        [Test]
+
+        public void Mouseover()
+        {
+            Actions actions = new Actions(driver);
+
+            actions.MoveToElement(driver.FindElement(By.XPath("//span[text()='Women']"))).MoveToElement(driver.FindElement(By.XPath("//span[text()='Tops']"))).Click();
+
+            //actions.MoveToElement(driver.FindElement(By.XPath("//span[text()='Tops']"))).Perform();
+
+            driver.FindElement(By.XPath("//span[text()='Hoodies & Sweatshirts']")).Click(); 
+        }
+
+    }
+}
---
+++ MagentoLuma/EditAccountInformation.cs
@@ -0,0 +1,42 @@
+﻿using MagentoLuma.Base;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagentoLuma
+{
+    public class EditAccountInformation : AutomationWrapper
+    {
+        [Test]
+        
+        public void EditAccount()
+        {
+
+            driver.FindElement(By.XPath("(//a[normalize-space()='Sign In'])[1]")).Click();
+            driver.FindElement(By.XPath("//input[@name = 'login[username]']")).SendKeys("rahul1309@gmail.com");
+            driver.FindElement(By.XPath("//input[@name='login[password]']")).SendKeys("Rahul@123");
+            driver.FindElement(By.XPath("//span[text()='Sign In']")).Click();
+            driver.FindElement(By.XPath("//button[@class='action switch']")).Click();
+            driver.FindElement(By.XPath("//a[text()='My Account']")).Click();
+            driver.FindElement(By.XPath("//a[text()='Account Information']")).Click();
+            driver.FindElement(By.Id("firstname")).Clear();
+            driver.FindElement(By.Id("firstname")).SendKeys("balaji");
+            driver.FindElement(By.Id("lastname")).Clear();
+            driver.FindElement(By.Id("lastname")).SendKeys("pawar");
+            driver.FindElement(By.Id("change-password")).Click();
+            driver.FindElement(By.Id("current-password")).SendKeys("Rahul@123");
+            driver.FindElement(By.Id("password")).SendKeys("Balaji@123");
+            driver.FindElement(By.Id("password-confirmation")).SendKeys("Balaji@123");
+            driver.FindElement(By.XPath("//span[text()='Save']")).Click();
+
+        }
+
+
+        }
+
+    }
+
